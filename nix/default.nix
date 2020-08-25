01174513a15f984e9bdd5ca8a65deab23a12094e
jschaul <jschaul@users.noreply.github.com>
2020-08-25 12:46:02 +0200

    attempt overlay - fail

--- nix/default.nix
+++ nix/default.nix
@@ -2,6 +2,8 @@ let
   sources = import ./sources.nix;
   pkgs = import sources.nixpkgs {
     config = {};
+    # TODO: overlay doesn't work yet
+    # overlays = [ (import ./overlay.nix) ];
   };
 in {
     env = pkgs.poetry2nix.mkPoetryEnv {
